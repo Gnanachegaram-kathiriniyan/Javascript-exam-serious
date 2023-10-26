b07133dfcb0a4c6eca11e2378bf9e713d1272eeb
Gnanachegaram-kathiriniyan <sughikaran002@gmail.com>
2023-10-26 10:37:52 +0530

    js exam serious

---
+++ practice.js
@@ -0,0 +1,40 @@
+// Q1
+var y={};
+
+// Q2
+function introduceYou(person)
+{
+console.log("Hello my name is "+person.name)
+}
+
+var person ={name:"kathir",age:20,introduceYou};
+console.log(person);
+
+// Q3
+var student = {StudentName:"kathir"};
+function greetStudent(student)
+{
+    console.log("Hello"+StudentName+"Welcome to the uki coding school")
+}
+console.log(greetStudent)
+
+//Q4
+console.log(person.age);
+console.log(person["age"]);
+
+// Q5
+person.email = ('john@example.com');
+console.log(person)
+
+// Q6
+delete student.StudentName
+
+// Q7
+
+// Q8
+Object.assign("studentCourse")
+
+// Q10
+var book={ title:"king",author:"karan",year:"2003"};
+
+// Q11
